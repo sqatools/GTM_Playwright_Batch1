bab4703bf38c6e80f3a011ee1db43332d7f95a1c
Varinderbatth1 <Varinderkbatth@gmail.com>
2025-07-16 19:16:24 -0400

    today's practice

---
+++ Varinder/If_condition_practice.py
@@ -0,0 +1,25 @@
+age = 10
+if age > 20:
+    print( "you are allow to drive without observation")
+else:
+    print("you need assistance")
+
+
+#compare two numbers
+a = 200
+b = 250
+print(a==b)
+if a==b:
+    print("bath values are equal")
+else:
+    print("No A is less b")
+
+varinder_age = 30
+jasmine_ge = 22
+if varinder_age < jasmine_ge:
+    print("varinder is younger the jasmine")
+else:
+    print("Jasmine is yunger then varinder")
+
+
+
---
+++ Varinder/Logical_Operator_practice.py
@@ -0,0 +1,17 @@
+# And operator
+num = 14
+if (num ==10) and (num ==14):
+        print("one condition is true")
+else:
+    print("because both conditions are not true condition gonna fall in else statement")
+
+
+# OR operator
+num1 = 16
+if (num1 == 30) or (num1 == 16):
+    print(" it will print this statement because at lest one condition is true")
+else:
+    print("one statement is false")
+
+#Multiple If conditions
+
