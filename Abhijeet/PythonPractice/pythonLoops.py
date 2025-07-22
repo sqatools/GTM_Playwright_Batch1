e000a8b9f5c45564abffaf1b325f463d9c22f524
Abhijeet Shrotre <abhijeetshrotre707@gmail.com>
2025-07-22 23:43:49 +0530

    If else loop conditions

---
+++ Abhijeet/PythonPractice/pythonLoops.py
@@ -0,0 +1,17 @@
+
+"""
+range(start value, end value, step value)
+->output will include the start value and exclude end value e.g. range (2,5,1): 2,3,4
+range(1,10,2):
+
+"""
+
+for i in range(2,7,1):
+    print(i)
+
+print('_'*50)
+
+#Print a program to print a table of any number
+
+for i in range(1, 11):
+    print(i, "*", 3, "=", i*3)
