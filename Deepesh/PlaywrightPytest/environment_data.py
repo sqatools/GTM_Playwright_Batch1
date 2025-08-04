3d9dafe71150498a3dafc849d4e6a21c1043d6c3
Deepesh <xovek11875@bitvoo.com>
2025-08-04 08:02:49 +0530

    Python with Playwright locators

---
+++ Deepesh/PlaywrightPytest/environment_data.py
@@ -0,0 +1,8 @@
+db_users_details = [
+    ('user1@gmail.com', 'user12345'),
+    ('user2@gmail.com', 'admin1234'),
+    ('user3@gmail.com', 'P@122344'),
+    ('user7@gmail.com', 'P@ssw0rd'),
+    ('user8@gmail.com', 'End@pass'),
+
+]
