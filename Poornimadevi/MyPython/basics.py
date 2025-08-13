d0e0d6fb20676f5352aa977307ae58200fad9330
poornimadevirengaraja <r.poornimadevi29@gmail.com>
2025-08-13 08:39:16 +0530

    frameworks

---
+++ Poornimadevi/MyPython/basics.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+print("Addition",a+b)
+
+p = q = r = 100
+print(p)
+print(q)
+print(r)
+
+
+x, y, z = 100, 200, 300
+print("x :", x)
+print("y :", y)
+print("z :", z)
+
+abc_xyz_hello_python_programming = 400
